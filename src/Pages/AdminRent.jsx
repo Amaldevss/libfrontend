7ccd76dadaf1d089a128baefbbcfc2425b995062
AkashChandran121233 <chandranakash92@gmail.com>
2025-01-10 22:41:38 +0530

    AdminRent.jsx

--- src/Pages/AdminRent.jsx
+++ src/Pages/AdminRent.jsx
@@ -9,7 +9,7 @@ function AdminRent() {
 
   useEffect(() => {
     // Fetch books from the backend when the component mounts
-    axios.get('http://localhost:3001/AdminRent')  // API endpoint
+    axios.get('https://libbackend-1.onrender.com/AdminRent')  // API endpoint
       .then((response) => {
         setRentBooks(response.data);  // Set the books in state
       })
